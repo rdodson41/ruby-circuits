2d572513d253422a88367d36c5797bddcc49e200
Richard E. Dodson <richard.elias.dodson@gmail.com>
2020-05-31 15:36:33 -0400

    Intall RuboCop and its extensions

--- Gemfile
+++ Gemfile
@@ -1,8 +1,21 @@
+# frozen_string_literal: true
+
 source('https://rubygems.org')
 
-# A runtime developer console and IRB alternative with powerful introspection
-# capabilities.
+# A runtime developer console and IRB alternative with powerful introspection capabilities.
 gem('pry')
 
 # Step-by-step debugging and stack navigation in Pry
 gem('pry-byebug')
+
+# A Ruby static code analyzer and formatter, based on the community Ruby style guide.
+gem('rubocop', require: false)
+
+# An extension of RuboCop focused on code performance checks.
+gem('rubocop-performance', require: false)
+
+# A RuboCop plugin for Rake
+gem('rubocop-rake', require: false)
+
+# Code style checking for RSpec files
+gem('rubocop-rspec', require: false)
--- lib/circuits.rb
+++ lib/circuits.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 require('matrix')
 
 Node = Struct.new(:id)
 
-Component = Struct.new(:id, :node_0, :node_1)
+Component = Struct.new(:id, :node0, :node1)
 
 class Resistor < Component
   attr_reader :resistance
 
-  def initialize(id, node_0, node_1, resistance)
-    super(id, node_0, node_1)
-    @resistance = resistance.to_f
+  def initialize(id, node0, node1, resistance)
+    super(id, node0, node1)
+    @resistance = Float(resistance)
   end
 
   def conductance
@@ -24,9 +26,9 @@ end
 class VoltageSource < Component
   attr_reader :voltage
 
-  def initialize(id, node_0, node_1, voltage)
-    super(id, node_0, node_1)
-    @voltage = voltage.to_f
+  def initialize(id, node0, node1, voltage)
+    super(id, node0, node1)
+    @voltage = Float(voltage)
   end
 
   def voltage_source?
@@ -34,73 +36,73 @@ class VoltageSource < Component
   end
 end
 
-Network = Struct.new(:nodes, :components) do
-  def offset
-    nodes.count - 1
-  end
+Network =
+  Struct.new(:nodes, :components) do
+    def offset
+      nodes.count - 1
+    end
 
-  def size
-    offset + components.count(&:voltage_source?)
-  end
+    def size
+      offset + components.count(&:voltage_source?)
+    end
 
-  def voltage_sources
-    components.select(&:voltage_source?)
-  end
+    def voltage_sources
+      components.select(&:voltage_source?)
+    end
 
-  def conductances
-    conductances = Matrix.zero(size)
+    def conductances
+      conductances = Matrix.zero(size)
 
-    components.each do |component|
-      node_0_index = nodes.index(component.node_0) - 1
-      node_1_index = nodes.index(component.node_1) - 1
-      case component
-      when Resistor
-        if node_0_index != -1
-          conductances[node_0_index, node_0_index] -= component.conductance
-        end
-        if node_1_index != -1
-          conductances[node_1_index, node_1_index] -= component.conductance
-        end
-        if node_0_index != -1 && node_1_index != -1
-          conductances[node_0_index, node_1_index] += component.conductance
-          conductances[node_1_index, node_0_index] += component.conductance
-        end
-      when VoltageSource
-        voltage_source_index = offset + voltage_sources.index(component)
-        if node_0_index != -1
-          conductances[node_0_index, voltage_source_index] -= 1
-          conductances[voltage_source_index, node_0_index] -= 1
-        end
-        if node_1_index != -1
-          conductances[node_1_index, voltage_source_index] += 1
-          conductances[voltage_source_index, node_1_index] += 1
+      components.each do |component|
+        node0_index = nodes.index(component.node0) - 1
+        node1_index = nodes.index(component.node1) - 1
+        case component
+        when Resistor
+          conductances[node0_index, node0_index] -= component.conductance if node0_index != -1
+          conductances[node1_index, node1_index] -= component.conductance if node1_index != -1
+          if node0_index != -1 && node1_index != -1
+            conductances[node0_index, node1_index] += component.conductance
+            conductances[node1_index, node0_index] += component.conductance
+          end
+        when VoltageSource
+          voltage_source_index = offset + voltage_sources.index(component)
+          if node0_index != -1
+            conductances[node0_index, voltage_source_index] -= 1
+            conductances[voltage_source_index, node0_index] -= 1
+          end
+          if node1_index != -1
+            conductances[node1_index, voltage_source_index] += 1
+            conductances[voltage_source_index, node1_index] += 1
+          end
         end
       end
+
+      conductances
     end
 
-    conductances
-  end
+    def currents
+      currents = Matrix.zero(size, 1)
 
-  def currents
-    currents = Matrix.zero(size, 1)
+      voltage_sources.each.with_index(offset) do |voltage_source, index|
+        currents[index, 0] = voltage_source.voltage
+      end
 
-    voltage_sources.each.with_index(offset) do |voltage_source, index|
-      currents[index, 0] = voltage_source.voltage
+      currents
     end
 
-    currents
+    def voltages
+      conductances.inverse * currents
+    end
   end
-
-  def voltages
-    conductances.inverse * currents
-  end
-end
 
 NODES = %w[V0 V1 V2 V3].map(&Node.method(:new))
 
-NETWORK = Network.new(NODES, [
-  VoltageSource.new('VA', NODES[0], NODES[1], 1),
-  Resistor.new('R1', NODES[1], NODES[2], 1000),
-  Resistor.new('R2', NODES[2], NODES[3], 1000),
-  Resistor.new('R3', NODES[3], NODES[0], 1000)
-])
+NETWORK = Network.new(
+  NODES,
+  [
+    VoltageSource.new('VA', NODES[0], NODES[1], 1),
+    Resistor.new('R1', NODES[1], NODES[2], 1000),
+    Resistor.new('R2', NODES[2], NODES[3], 1000),
+    Resistor.new('R3', NODES[3], NODES[0], 1000)
+  ]
+)
